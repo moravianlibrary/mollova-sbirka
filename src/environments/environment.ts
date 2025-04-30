b58b7f8e80e460c4e6e8d571a509d5f2a74c47f6
Martin Řehánek <martin.rehanek@gmail.com>
2025-04-30 11:59:06 +0200

    try preventing runtime error when problems with @ngx-env/builder and import.meta.env (or variable) not being defined

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,20 @@
-export const environment = {
-
+function getEnv(key: string, fallback: string): string {
+    return typeof import.meta.env !== 'undefined' &&
+      typeof import.meta.env[key] !== 'undefined'
+      ? import.meta.env[key]
+      : fallback;
+  }
+  
+  export const environment = {
     devMode: false,
     environmentName: 'production',
     environmentCode: 'p',
-    ngApTest: import.meta.env["NG_APP_TEST"] || 'not-found-for-environment.ts',
-
-    krameriusBaseUrl: import.meta.env["NG_APP_KRAMERIUS_URL"] || 'https://api.kramerius.mzk.cz/search',
-    elasticBaseUrl: import.meta.env["NG_APP_ELASTIC_URL"] || 'http://localhost:9200/moll',
-    elasticLogin: import.meta.env["NG_APP_ELASTIC_LOGIN"] || 'FIXME:LOGIN',
-    elasticPassword: import.meta.env["NG_APP_ELASTIC_PASSWORD"] || 'FIXME:PASSWORD',
-
-};
+  
+    ngApTest: getEnv('NG_APP_TEST', 'not-found-for-environment.ts'),
+  
+    krameriusBaseUrl: getEnv('NG_APP_KRAMERIUS_URL', 'https://api.kramerius.mzk.cz/search'),
+    elasticBaseUrl: getEnv('NG_APP_ELASTIC_URL', 'http://localhost:9200/moll'),
+    elasticLogin: getEnv('NG_APP_ELASTIC_LOGIN', 'FIXME:LOGIN'),
+    elasticPassword: getEnv('NG_APP_ELASTIC_PASSWORD', 'FIXME:PASSWORD'),
+  };
+  
