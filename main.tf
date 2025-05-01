de5433fd886e7464fcbd9ad12e0592db250308ab
Slavik Svyrydiuk <slavik@svyrydiuk.eu>
2025-05-01 12:06:48 +0200

    fix: docker env vars

--- main.tf
+++ main.tf
@@ -8,6 +8,28 @@ terraform {
   }
 }
 
+variable "APP_DEV_MODE" {
+  type = string
+}
+variable "APP_ENV_CODE" {
+  type = string
+}
+variable "APP_API_URL" {
+  type = string
+}
+variable "APP_KRAMERIUS_URL" {
+  type = string
+}
+variable "APP_ELASTIC_URL" {
+  type = string
+}
+variable "APP_ELASTIC_LOGIN" {
+  type = string
+}
+variable "APP_ELASTIC_PASSWORD" {
+  type = string
+}
+
 variable "docker_host_uri" {
   type = string
 }
@@ -23,7 +45,6 @@ variable "deploy_domain" {
 variable "docker_container_name" {
   type = string
 }
-
 
 variable "ghcr_username" {
   type = string
@@ -55,7 +76,7 @@ resource "docker_container" "moll_frontend" {
   name              = var.docker_container_name
   must_run          = true
   publish_all_ports = true
-  # restart           = "always" # default "no"
+  env               = ["APP_DEV_MODE=${var.APP_DEV_MODE}", "APP_ENV_CODE=${var.APP_ENV_CODE}", "APP_API_URL=${var.APP_API_URL}", "APP_KRAMERIUS_URL=${var.APP_KRAMERIUS_URL}", "APP_ELASTIC_URL=${var.APP_ELASTIC_URL}", "APP_ELASTIC_LOGIN=${var.APP_ELASTIC_LOGIN}", "APP_ELASTIC_PASSWORD=${var.APP_ELASTIC_PASSWORD}"]
 
   labels {
     label = "traefik.http.routers.${var.docker_container_name}.rule"
